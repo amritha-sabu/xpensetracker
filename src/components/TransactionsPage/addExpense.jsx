8620cf40394259b5c52dcaf4b8e38c22dd0ae3e3
amritha-sabu <amritha.g.sabu@gmail.com>
2024-12-23 16:40:49 +0530

    Section to add new expenses added

---
+++ src/components/TransactionsPage/addExpense.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setExpense } from "../../redux/expenceSlice";
+
+const AddNewExpense = () => {
+    const [newExpense, setNewExpense] = useState({
+        'expense-name' : '',
+        'category-select' : '',
+        'expense-amount' : '',
+    });
+    const dispatch = useDispatch();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewExpense({
+            ...newExpense,
+            [name] : value
+        })
+    };
+
+    const handleAddNewExpense = (e) => {
+        e.preventDefault();
+        dispatch(setExpense(newExpense));
+
+        setNewExpense({
+            'expense-name' : '',
+            'category-select' : '',
+            'expense-amount' : '',
+        });
+    };
+
+    return (
+        <div>
+            <div id="title">New Expense Form</div>
+
+            <form id="expense-form1" onSubmit={(e) => handleAddNewExpense(e)} >
+                <label htmlFor="expense-name">Expense Name</label>
+                <input type='text' name="expense-name" value={newExpense['expense-name']} onChange={(e) => handleChange(e)} />
+                <label htmlFor="category-select">Select a Category</label>
+                <select name="category-select" value={newExpense['category-select']} onChange={(e) => handleChange(e)} >
+                    <option value="" disabled>Select</option>
+                    <option value='Food' >Food</option>
+                    <option value='Travel' >Travel</option>
+                    <option value='Entertainment' >Entertainment</option>
+                    <option>Other</option>
+                </select>
+                <label htmlFor="expense-amount">Expense Amount</label>
+                <input type='number' name="expense-amount" value={newExpense['expense-amount']} onChange={(e) => handleChange(e)} />
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddNewExpense;
