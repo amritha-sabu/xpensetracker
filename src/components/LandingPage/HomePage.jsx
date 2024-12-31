e1bd095e8d30b664ee2c84ccc718e84c11a89982
amritha-sabu <amritha.g.sabu@gmail.com>
2024-12-31 19:07:42 +0530

    Home Page CSS

--- src/components/LandingPage/HomePage.jsx
+++ src/components/LandingPage/HomePage.jsx
@@ -3,7 +3,8 @@ import styles from './homePage.module.css';
 const HomePage = () => {
     return (
         <div className={styles.body}>
-            <h1>Take Control of Your Finances</h1>
+            <h1 className={styles.heading}>Take Control of Your Finances</h1>
+            <p className={styles.paragraph}>With out app you can easily track your income and expenses, set financila goals, and make informed decisions about your money.</p>
             <button>Get Started</button>
         </div>
     );
