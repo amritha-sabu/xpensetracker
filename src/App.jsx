a2f7adfbb2e43ec73b98e5fe0fea32fb4b89f93f
amritha-sabu <amritha.g.sabu@gmail.com>
2024-12-31 19:10:23 +0530

    Navigate to Budget Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TransactionPage from './components/TransactionsPage/TransactionPage';
 import HomePage from './components/LandingPage/HomePage';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/budget" element={<LandingPage />} />
           <Route path='/transactions' element={<TransactionPage/>} />
         </Routes>
       </BrowserRouter>
--- src/components/LandingPage/HomePage.jsx
+++ src/components/LandingPage/HomePage.jsx
@@ -1,11 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './homePage.module.css';
 
 const HomePage = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = () => {
+        navigate('/budget');
+    };
+
     return (
         <div className={styles.body}>
             <h1 className={styles.heading}>Take Control of Your Finances</h1>
             <p className={styles.paragraph}>With out app you can easily track your income and expenses, set financila goals, and make informed decisions about your money.</p>
-            <button>Get Started</button>
+            <button onClick={handleNavigation()}>Get Started</button>
         </div>
     );
 };
